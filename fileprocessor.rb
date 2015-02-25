2b84c8fa06662dc6289daadfbdb4e0a697e8c799
Kim Foale <kim@alliscalm.net>
2015-02-25 11:41:12 +0000

    Server script outline done

--- fileprocessor.rb
+++ fileprocessor.rb
@@ -1,6 +1,62 @@
 require "./models"
 
-a = Audio.get(28)
-input = a.source.path
-output = File.dirname(input)
-`./windDet -i #{input} -o #{output}/output`
+class ProcessorQueue
+
+  def initialize
+    # Start the queue
+    puts "Queue starting."
+    @running = true
+    # Get the next ticket
+    next_ticket
+  end
+
+  def next_ticket
+    # Find next unprocessed ticket sorted by number order
+    ticket = Audio.first(:processed => false)
+    if(ticket)
+      # If there's a ticket to process, go do that
+      @ticket = ticket.id
+      puts "Next ticket: #{@ticket}"
+      process(@ticket)
+    else
+      # If not, stop the queue
+      puts "No files to process. Shutting down."
+      @running = false
+    end
+  end
+
+  def process(id)
+    # Process a file given an ID
+    a = Audio.get(id)
+    puts "#{id}: Starting processing"
+    input = a.source.path
+    puts "Source path is #{input}. Starting..."
+    output = File.dirname(input)
+    # Run the windDet binary
+    `./windDet -i #{input} -o #{output}/output`
+    puts "#{a.id}: Processing complete"
+    # Mark it as complete in the database
+    a.processed = true
+    a.save
+    # Check for the next file
+    next_ticket
+  end
+
+  def check_if_running
+    # To be used post-form submit to check the queue is running.
+    # If it's not, process it now.
+    if @running == false
+      @running = true
+      next_ticket 
+    end
+  end
+
+  def current_ticket
+    # Output current ticket status
+    @ticket
+  end
+
+end
+
+# Start the thing up
+queue = ProcessorQueue.new
